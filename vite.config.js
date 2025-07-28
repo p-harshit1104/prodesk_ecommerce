65c0414829309268cc37c45531baef635e1bbe16
p-harshit1104 <harshitp110604@gmail.com>
2025-07-28 15:36:06 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shopping_buddy_cart/',
+  
   plugins: [react(), tailwindcss()],
 })
