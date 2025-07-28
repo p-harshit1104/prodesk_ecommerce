223554b0f93f987d4719c0b204e07754bf6ed0a9
p-harshit1104 <harshitp110604@gmail.com>
2025-07-28 15:41:43 +0530

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename="/shopping_buddy_cart">
+  <BrowserRouter>
       <Provider store={Store}>
         <App />
         <ToastContainer/>
